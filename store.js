1d87e2df6aaf8980ab35b4eb96c90e9eea295bff
travis flake <tflake83@gmail.com>
2023-02-19 19:00:31 -0500

    redux reducer config storage

---
+++ store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import basketReducer from './features/basketSlice'
+import restaurantReducer from './features/restaurantSlice'
+//USED TO COMBINE ALL SLICES GLOBALLY
+export const store = configureStore({
+    reducer: {
+        basket: basketReducer,
+        restaurant: restaurantReducer
+    },
+})
